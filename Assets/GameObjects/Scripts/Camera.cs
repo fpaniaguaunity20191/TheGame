5eb402ec9b2fb225cdf94566b8cd2276ace56727
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-03-28 14:03:39 +0100

    Initial commit

---
+++ Assets/GameObjects/Scripts/Camera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float maxDistanceToPlayer;
+    private float sqrMaxDistanceToPlayer;
+    private float sqrDistanceToPlayer;
+    private GameObject player;
+    private float yPos;
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        yPos = transform.position.y;
+    }
+    private void Start()
+    {
+        sqrMaxDistanceToPlayer = maxDistanceToPlayer * maxDistanceToPlayer;
+    }
+    void Update()
+    {
+        transform.LookAt(player.transform);
+        sqrDistanceToPlayer = GetDistance();
+        if (sqrDistanceToPlayer > sqrMaxDistanceToPlayer)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+            if (transform.position.y < yPos)
+            {
+                transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
+            }
+        }
+    }
+    private float GetDistance()
+    {
+        float distance = Vector3.SqrMagnitude(player.transform.position - transform.position);
+        return distance;
+    }
+}
